ab7f8bf9d3076a9408fffae45954cff0f0ce788b
castromir <caiocastromir@gmail.com>
2026-01-09 10:48:02 -0300

    Inputs do login funcionando, e persistindo valor

--- src/app/(auth)/login/form.tsx
+++ src/app/(auth)/login/form.tsx
@@ -3,121 +3,77 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label"
+import { SelectGroup, SelectLabel } from "@/components/ui/select";
+
 import * as React from "react"
-import { CheckIcon, ChevronsUpDownIcon } from "lucide-react"
-import { cn } from "@/lib/utils"
+
+import { useState } from 'react';
 
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
-const users = [
-  {
-    value: "barto",
-    label: "Bartô",
-  },
-  {
-    value: "maite",
-    label: "Maitê",
-  },
-  {
-    value: "nyx",
-    label: "Nyx",
-  },
-  {
-    value: "oscar",
-    label: "Oscar",
-  },
-  {
-    value: "rogerio",
-    label: "Rogério",
-  },
-  {
-    value: "xavier",
-    label: "Xavier",
-  },
-]
-
-export function LoginCombobox() {
-  const [open, setOpen] = React.useState(false)
-  const [value, setValue] = React.useState("")
+export function SelectLogin( {
+    value,
+  onChange,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+}) {
+  
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full justify-between"
-        >
-          {value
-            ? users.find((user) => user.value === value)?.label
-            : "Selecione o usuário..."}
-          <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[--radix-popover-trigger-width] p-0">
-        <Command>
-          <CommandInput placeholder="Pesquisar usuário..." />
-          <CommandList>
-            <CommandEmpty>Nenhum usuário encontrado.</CommandEmpty>
-            <CommandGroup>
-              {users.map((user) => (
-                <CommandItem
-                  key={user.value}
-                  value={user.value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue)
-                    setOpen(false)
-                  }}
-                >
-                  <CheckIcon
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === user.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {user.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <Select value={value} onValueChange={onChange}>
+      <SelectTrigger className="w-full justify-between">
+        <SelectValue placeholder="Selecione o usuário" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Usuários</SelectLabel>
+          <SelectItem value="barto">Bartô</SelectItem>
+          <SelectItem value="maite">Maitê</SelectItem>
+          <SelectItem value="nyx">Nyx</SelectItem>
+          <SelectItem value="rogerio">Rogério</SelectItem>
+          <SelectItem value="xavier">Xavier</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   )
 }
 
+
 export const LoginForm = () => {
-    const onSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log('Login!');
-    }
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const onSubmit = (e: React.FormEvent) => {
+      e.preventDefault();
+      console.log(usuario, senha);
+  }
     return (
-        <form className="space-y-8 w-[400px]"> 
-            <div className="grid w-full gap-1.5">
-                <Label htmlFor="email">Usuário</Label>
-                <LoginCombobox />
-            </div>
+      <form onSubmit={onSubmit} className="space-y-8 w-[400px]"> 
+        <div className="grid w-full gap-1.5">
+          <Label htmlFor="usuario">Usuário</Label>
+          <SelectLogin value={usuario} onChange={setUsuario}/>
+        </div>
 
-            <div className="grid w-full items-center gap-1.5">
-                <Label htmlFor="senha">Senha</Label>
-                <Input id="senha" type="senha" required />
-            </div> 
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="senha">Senha</Label>
+          <Input
+            id="senha"
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            required
+          />
+        </div> 
 
-            <div className="grid w-full items-center">
-                <Button>Login</Button>  
-            </div>
-        </form>
+        <div className="grid w-full items-center">
+          <Button>Login</Button>  
+        </div>
+      </form>
     )
 }
